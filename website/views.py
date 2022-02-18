398371717535f8938e86b2807e0331034c4ea822
Oleg Brezitskyy <brezitskyyoleg@gmail.com>
2022-02-18 01:24:28 +0100

    ➕ Añadidas las vistas de "Lecturas" y "Añadir lectura"

--- website/views.py
+++ website/views.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
+
+
+@views.route("/readings")
+@login_required
+def patient_home():
+    return render_template("patient_home.html", user=current_user)
+
+
+@views.route("/add-reading")
+@login_required
+def patient_add_reading():
+    return render_template("patient_add_reading.html", user=current_user)
