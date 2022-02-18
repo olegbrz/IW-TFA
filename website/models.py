538f7c3ff0feb6817987f208010bfbd7df042bef
Oleg Brezitskyy <brezitskyyoleg@gmail.com>
2022-02-18 20:33:22 +0100

    ✏️ Minor model fixes

--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import desc
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -16,7 +17,9 @@ class Paciente(db.Model, UserMixin):
     def get_id(self):
         return self.nif
 
-    # readings = db.relationship("Lectura")
+    readings = db.relationship("Lectura", order_by="desc(Lectura.fecha_hora)")
+    prescriptions = db.relationship("Receta", order_by="desc(Receta.fecha_inicio)")
+    medico_nif = db.Column(db.Integer, db.ForeignKey("Medico.nif"))
 
 
 class Medico(db.Model, UserMixin):
@@ -27,33 +30,37 @@ class Medico(db.Model, UserMixin):
     email = db.Column(db.String(45), unique=True)
     password = db.Column(db.String(500))
 
+    pacientes = db.relationship("Paciente")
+
     def get_id(self):
         return self.nif
 
 
 class Lectura(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "Lectura"
+    idLectura = db.Column(db.Integer, primary_key=True)
     fecha_hora = db.Column(db.DateTime(timezone=True), default=func.now())
     ta_sistolica = db.Column(db.Integer)
     ta_diastolica = db.Column(db.Integer)
     ppm = db.Column(db.Integer)
-    notes = db.Column(db.Text)
+    notas = db.Column(db.Text)
     notas_medico = db.Column(db.Text)
     medicacion_tomada = db.Column(db.Boolean)
 
-    paciente_nif = db.Column(db.Integer, db.ForeignKey("paciente.nif"))
+    paciente_nif = db.Column(db.Integer, db.ForeignKey("Paciente.nif"))
 
 
-class Receta(db.model):
+class Receta(db.Model):
+    __tablename__ = "Receta"
     idReceta = db.Column(db.Integer, primary_key=True)
     fecha_inicio = db.Column(db.Date)
     fecha_fin = db.Column(db.Date)
     medicamento = db.Column(db.String(45))
     principio_activo = db.Column(db.String(45))
-    posología = db.Column(db.String(45))
+    posologia = db.Column(db.String(45))
 
-    paciente_nif = db.Column(db.Integer, db.ForeignKey("paciente.nif"))
-    medico_nif = db.Column(db.Ingeger, db.ForeignKey("medico.nif"))
+    paciente_nif = db.Column(db.Integer, db.ForeignKey("Paciente.nif"))
+    medico_nif = db.Column(db.Integer, db.ForeignKey("Medico.nif"))
 
     def get_id(self):
         return self.idReceta
